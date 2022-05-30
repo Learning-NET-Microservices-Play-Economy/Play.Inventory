71c861e4324461d5f0c1288d203635f41e573bcd
Mozart Alkhateeb <mozart.alkhateeb@gmail.com>
2022-05-30 23:03:29 +0300

    feat: added circuit breaker

--- Play.Inventory.Service/Program.cs
+++ Play.Inventory.Service/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Mozart.Play.Common.MongoDb;
-using Mozart.Play.Common.Settings;
 using Mozart.Play.Inventory.Service.Clients;
 using Mozart.Play.Inventory.Service.Entities;
 using Polly;
@@ -9,44 +8,61 @@ namespace Mozart.Play.Inventory.Service;
 
 public class Program
 {
-    private static ServiceSettings serviceSettings;
-
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
 
         // Add services to the container.
-        serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-        builder.Services.AddMongo();
-        builder.Services.AddMongoRepository<InventoryItem>("inventory.items");
+        services.AddMongo();
+        services.AddMongoRepository<InventoryItem>("inventory.items");
 
         Random jitterer = new Random();
 
-        builder.Services.AddHttpClient<CatalogClient>(client =>
+        services.AddHttpClient<CatalogClient>(client =>
         {
             client.BaseAddress = new Uri("https://localhost:7273");
         })
         .AddTransientHttpErrorPolicy(builder =>
             builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-                5,
-                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+                5, // Number of retries after failing
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)), // Wait time before another retry
                 onRetry: (outcome, timeSpan, retryAttempt) =>
                 {
-                    // do dome logging here
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                        .LogWarning($"Delaying for {timeSpan.TotalSeconds} seconds, then making retry {retryAttempt}");
                 })
+        )
+        .AddTransientHttpErrorPolicy(builder =>
+        builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+            3, // Allow up to three fails
+            TimeSpan.FromSeconds(15), // Block requests for 13 seconds
+            onBreak: (outcome, timeSpan) =>
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()?
+                    .LogWarning($"Opening the circuit for {timeSpan.TotalSeconds} seconds...");
+            },
+            onReset: () =>
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()?
+                    .LogWarning($"Closing the circuit...");
+            }
+            )
         )
         .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
-        builder.Services.AddControllers(options =>
+        services.AddControllers(options =>
         {
             options.SuppressAsyncSuffixInActionNames = false;
         });
 
-        builder.Services.AddControllers();
+        services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
 
         var app = builder.Build();
 
